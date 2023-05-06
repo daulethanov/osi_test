3a075a52dc37228aa5a13633da7785252d79e2f5
alihan <aliz1233773@gmail.com>
2023-05-07 00:57:20 +0600

    07.05.23 v2

--- backend/runserver.py
+++ backend/runserver.py
@@ -2,7 +2,7 @@ from services import create_app
 
 app = create_app()
 
-# SQLALCHEMY_DATABASE_URI='postgresql://postgres:1234@database:5432/dbname'
+# SQLALCHEMY_DATABASE_URI='sqlite:///db.db'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
